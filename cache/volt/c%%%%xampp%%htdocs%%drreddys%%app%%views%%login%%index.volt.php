3b3cbccea9beda074b68652cf5840ecb12ed045f
vinita <sd4@consultlane.com>
2019-12-27 15:32:45 +0530

    updates

---
+++ cache/volt/c%%%%xampp%%htdocs%%drreddys%%app%%views%%login%%index.volt.php
@@ -0,0 +1,57 @@
+<?php //echo 'login';exit; ?>
+<!-- =============================== main container =============================== -->
+
+<!-- Main content -->
+<div class="containerfrmenl">
+        <div class="loginleftmn"></div>
+    
+	<section id="contentmnfrm">
+		<div class="logo_file"><img src="img/logo.png"></div>
+       <div class="mainloginfrmrl">  
+        <h1>Volody Insider Trading</h1>
+        <div class="progress-indeterminate">
+                  <div class="indeterminate"></div>
+        </div>   
+		<form class="login-form floatright" method="post" action="login/loginchec" autocomplete="off" id="ValidateLogin">
+			
+            <!-- <div class="thumbnail"><img src="img/board-meeting-left.png" class="thmbimgbtn"></div> -->
+			<div class="maininputcl">
+				<input type="text" placeholder="Username" required="" id="username" name="youremail"  value="" autocomplete="off"/>
+			</div>
+			<div class="maininputcl">
+				<input type="password" placeholder="Password" required="" id="password" name="password"  value=""
+				 autocomplete="off" />
+			</div>
+			<div class="maininputclbtn">
+				<input type="submit" value="Log in" class="loginnowbtn"/>
+				<a href="forgotpassword">Lost your password</a>
+                <div class="clearelement"></div>
+			</div>
+		</form><!-- form -->
+           <div class="errorelement"><div class="geterrorelemttxt"></div></div>
+			<div class="copyright">
+			    <div class="">© 2016 All Rights Reserved. Made with 
+			        <i class="fa fa-heart heartcolor"></i> 
+			        in Mumbai.
+			        </div>
+			    <div class="clearelement"></div>
+			</div>
+           
+        <div class="clearelement"></div>
+<!-- 		<div class="button minprogreabbr">
+            <div class="progressnew">
+                <div class="progressnew-bar">
+                    <div class="progressnew-shadow"></div>
+                </div>
+            </div>    
+            <div class="errorelement"><div class="geterrorelemttxt"></div></div>
+            <div class="tooltipmn">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            <a href="mylogin">Request for Demo</a>
+		</div> -->
+		<!-- button -->
+        </div> 
+	</section><!-- content -->
+    <div class="loginrightmn"></div>
+</div><!-- container -->
+<!-- /main content -->
+		
