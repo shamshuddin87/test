2acf86f23fbaddc381f230a26050ef021e4cf2ac
Aniket Varma <sd6@consultlane.com>
2021-04-09 19:16:16 +0530

    commit

--- app/library/Htmlelements.php
+++ app/library/Htmlelements.php
@@ -225,7 +225,8 @@ class Htmlelements extends Component
           <table cellpadding="0" cellspacing="0" border="0" width="100%">
             <tr>
               <td align="center" colspan="2">
-                <p style="text-align:center;line-height: 20px;color:#333; font-size:15px; font-family:\'Roboto\',Arial,Helvetica,sans-serif;">In case of login issues, please contact </br> techease (techease@drreddys.com)</p>
+                <p style="text-align:center;line-height: 20px;color:#333; font-size:15px; font-family:\'Roboto\',Arial,Helvetica,sans-serif;">In case of login issues, please contact</p>
+                <p style="text-align:center;line-height: 20px;color:#333; font-size:15px; font-family:\'Roboto\',Arial,Helvetica,sans-serif;">techease (techease@drreddys.com)</p>
               </td>
             </tr>
               <tr>
